5a21395d3d43795ec189bcd32714b050f9f6a16b
qingmingxin <shanxueqiang@mail.nwpu.edu.cn>
2025-05-23 03:25:11 +0000

    update: complete the storagestatus class  define

--- model/src/main/java/com/qingmingxin/DFSystem/model/entity/StorageStatus.java
+++ model/src/main/java/com/qingmingxin/DFSystem/model/entity/StorageStatus.java
@@ -1,5 +1,6 @@
 package com.qingmingxin.DFSystem.model.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -20,6 +21,20 @@ public class StorageStatus extends BaseEntity {
     @TableField(value="time_stamp")
     private Date timestamp;
 
-    
+    @Schema(description = "CPU利用率")
+    @TableField(value = "cpu_usage")
+    private Double cpuUsage;
+
+    @Schema(description = "内存利用率")
+    @TableField(value = "memory_usage")
+    private Double memoryUsage;
+
+    @Schema(description = "总内存大小")
+    @TableField(value = "memory_total")
+    private long memoryTotal;
+
+    @Schema(description = "已用内存")
+    @TableField(value = "memory_used")
+    private long memoryUsed;
 
 }
